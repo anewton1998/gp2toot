f84f34f53d99293e34bdc1fd1f3575b7472492de
rcode3 <andy@hxr.us>
2019-04-26 15:59:59 -0400

    first non-erroring version

---
+++ etc/test_config.rb
@@ -0,0 +1,6 @@
+Gp2Toot.configure do |config|
+  config.loggerOut = 'gp2toot.out'
+  config.bearerToken = '9adb636d6343322795e10fa4411dd68fcd08d7088989b1e5bd3a7e47eac59ee1'
+  config.acct = 'botsington_sr'
+  config.loggerLevel = 'debug'
+end
